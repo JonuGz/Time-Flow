ee3826a647bad8b736d748b17e6c5933b7628055
JonuGonzalez <149194997+JonuGz@users.noreply.github.com>
2023-11-02 00:03:01 -0600

    Ajuste vision de movil

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ function cambiarContenido() {
     var parrafo = document.getElementById("TFM");
     var windowWidth = window.innerWidth;
 
-    if (windowWidth <= 600) {
+    if (windowWidth <= 812) {
         parrafo.textContent = "TFM";
     } else {
         parrafo.textContent = "TimeFlowMaster";
     }
 }
 
-window.addEventListener("resize", cambiarContenido);
 cambiarContenido(); // Llamar a la función al cargar la página para establecer el contenido inicial.
